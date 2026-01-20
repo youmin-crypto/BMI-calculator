458dc7b3439442d786c0166951ad48dce9055a75
youmin-crypto <you000957@gmail.com>
2026-01-20 14:59:55 +0630

    Update app.py

--- app.py
+++ app.py
@@ -14,9 +14,9 @@ if st.button("ရေတွက်မှုကို စတင်ပါ"):
     # မှတ်ချက် - ၁ သန်းအထိ တစ်ခုချင်းစီ screen ပေါ်တင်ပြရင် browser လေးသွားနိုင်လို့ 
     # အဆင့်လိုက်ပြပေးပါမယ်
     count = 0
-    limit = 1000000
+    limit = 100000000
     
-    for i in range(0, limit + 1, 10000): # ၁ သောင်းစီ ခုန်ပြီး progress ပြပါမယ်
+    for i in range(0, limit + 1, 100000): # ၁ သောင်းစီ ခုန်ပြီး progress ပြပါမယ်
         count = i
         percent_complete = i / limit
         progress_bar.progress(percent_complete)
@@ -24,7 +24,6 @@ if st.button("ရေတွက်မှုကို စတင်ပါ"):
     
     end_time = time.time()  # ပြီးဆုံးချိန်ကို မှတ်သားသည်
     duration = end_time - start_time # ကြာချိန်ကို တွက်သည်
-    
     st.success(f"✅ ရေတွက်လို့ ပြီးပါပြီ!")
     st.balloons() # အောင်မြင်မှု အထိမ်းအမှတ် မိုးပျံပူဖောင်းလေးများ
     
